54b292b0078dd567b5c1fb37c073c576e6772faf
Nathan Glover <nathan@glovers.id.au>
2016-06-12 22:51:11 +0800

    PEP8 reformat

--- overStat.py
+++ overStat.py
@@ -1,7 +1,5 @@
 import urllib3
 import simplejson as json
-import csv
-import sys
 import re
 
 
@@ -19,7 +17,10 @@ def requestStats():
     _region = input()
 
     _http = urllib3.PoolManager()
-    _request = _http.request('GET', 'https://api.lootbox.eu/' + _platform + '/' + _region + '/' + _username + '/heroes')
+    _request = _http.request('GET', 'https://api.lootbox.eu/' +
+                             _platform + '/' +
+                             _region + '/' +
+                             _username + '/heroes')
     _json_data = json.loads(_request.data)
 
     for item in _json_data:
@@ -31,21 +32,29 @@ def compareStats(_user1, _user2, _platform, _region):
     _username2 = re.sub('#', '-', _user2, 0)
 
     _http = urllib3.PoolManager()
-    _request1 = _http.request('GET', 'https://api.lootbox.eu/' + _platform + '/' + _region + '/' + _username1 + '/profile')
-    _request2 = _http.request('GET', 'https://api.lootbox.eu/' + _platform + '/' + _region + '/' + _username2 + '/profile')
+
+    _request1 = _http.request('GET', 'https://api.lootbox.eu/' +
+                              _platform + '/' +
+                              _region + '/' +
+                              _username1 + '/profile')
+    _request2 = _http.request('GET', 'https://api.lootbox.eu/' +
+                              _platform + '/' +
+                              _region + '/' +
+                              _username2 + '/profile')
+
     _json_data1 = json.loads(_request1.data)
     _json_data2 = json.loads(_request2.data)
 
-    print('Username: ',_json_data1['data']['username'],'\t',
-          'Playtime: ',_json_data1['data']['playtime'],'\t',
-          'Wins: ',_json_data1['data']['games']['wins'],'\t',
-          'Lost: ',_json_data1['data']['games']['lost'],'\t',
-          'Win Percentage: ',_json_data1['data']['games']['win_percentage']
+    print('Username: ', _json_data1['data']['username'], '\t',
+          'Playtime: ', _json_data1['data']['playtime'], '\t',
+          'Wins: ', _json_data1['data']['games']['wins'], '\t',
+          'Lost: ', _json_data1['data']['games']['lost'], '\t',
+          'Win Percentage: ', _json_data1['data']['games']['win_percentage']
           )
-    print('Username: ', _json_data2['data']['username'],'\t',
-          'Playtime: ', _json_data2['data']['playtime'],'\t',
-          'Wins: ', _json_data2['data']['games']['wins'],'\t',
-          'Lost: ', _json_data2['data']['games']['lost'],'\t',
+    print('Username: ', _json_data2['data']['username'], '\t',
+          'Playtime: ', _json_data2['data']['playtime'], '\t',
+          'Wins: ', _json_data2['data']['games']['wins'], '\t',
+          'Lost: ', _json_data2['data']['games']['lost'], '\t',
           'Win Percentage: ', _json_data2['data']['games']['win_percentage']
           )
 
@@ -54,4 +63,4 @@ if __name__ == "__main__":
     # Disable cert warnings
     urllib3.disable_warnings()
 
-    compareStats('UserName1#1234', 'UserName2#1234', 'pc', 'us')
+    compareStats('GloverDude#1471', 'adR#1700', 'pc', 'us')
